63cf040871c6397cba3113a296286d25015029d2
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-02 23:24:38 -0500

    Create ejercicio-nro20.py

---
+++ ejercicio-nro20.py
@@ -0,0 +1,17 @@
+#calculadora nro20
+#hallar la discriminante de una ecuacion
+
+#declaracion de valores
+a,b,c,discriminante=0.0,0.0,0.0,0.0
+
+#calculadora
+a=7
+b=17
+c=429
+discriminante=(b**2)-(4*a*c)
+
+#mostrar datos
+print("el valor de a es:", a)
+print("el valor de b es:", b)
+print("el valor de c es:", c)
+print("la discriminante de la ecuacion es:", discriminante)
