fe175e34d5ef040b6ed96559174af67a61ffba4b
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-02 22:44:50 -0500

    Create ejercicio-nro6.py

---
+++ ejercicio-nro6.py
@@ -0,0 +1,17 @@
+#calculadora nro6
+#hallar el perimetro y area de un hexagono
+
+#declaracion de variables
+base_h,apotema,perimetro_hexagono,area_hexagono=0.0,0.0,0.0,0.0
+
+#calculadora
+base_h=7.53
+apotema=5.69
+perimetro_hexagono=6*base_h
+area_hexagono=(perimetro_hexagono*apotema)/2
+
+#mostar datos
+print("la base del hexagono es:", base_h)
+print("su apotema es:", apotema)
+print("el prerimetro del hexagono es:", perimetro_hexagono)
+print("el area del hexagono es:", area_hexagono)
