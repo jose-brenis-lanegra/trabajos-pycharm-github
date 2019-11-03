3659dfa40c04216f92737ceb6d3846c8d8c70c18
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-02 22:43:58 -0500

    Create ejercicio-nro5.py

---
+++ ejercicio-nro5.py
@@ -0,0 +1,17 @@
+#calculadora nro5
+#hallar el area de un trapecio
+
+#declaracion de variables
+base_mayor,base_menor,altura,area_trapecio=0.0,0.0,0.0,0.0
+
+#calculadora
+base_mayor=21.89
+base_menor=16.42
+altura=31.70
+area_trapecio=(altura*(base_mayor+base_menor))/2
+
+#mostrar datos
+print("la base mayor es:", base_mayor)
+print("la base menor es:", base_menor)
+print("la altura del trapecio es:",altura)
+print("el area del trapecio es:", area_trapecio)
