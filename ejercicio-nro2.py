00a822a713494d563a314f04c07cbf86d921e3d7
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-02 22:39:38 -0500

    Create ejercicio-nro2.py

---
+++ ejercicio-nro2.py
@@ -0,0 +1,15 @@
+#calculadora nro2
+#hallar el perimetro y area de un cuadrado
+
+#declaracion de variables
+lado,perimetro_cuadrado,area_cuadrado=0.0,0.0,0.0
+
+#calculadora
+lado=13.06
+perimetro_cuadrado=4*lado
+area_cuadrado=lado**2
+
+#mostrar datos
+print("el lado es:", lado)
+print("el perimetro del cuadrado es:", perimetro_cuadrado)
+print("el area del cuadrado es:", area_cuadrado)
