bc1bd7537e4d731285e627cd516c99658809b8ce
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-02 22:43:09 -0500

    Create ejercicio-nro4.py

---
+++ ejercicio-nro4.py
@@ -0,0 +1,15 @@
+#calculadora nro4
+#hallar el perimetro del romboide
+
+#declaracion de variables
+base,altura,perimetro_romboide=0.0,0.0,0.0
+
+#calculadora
+base=7.43
+altura=3.16
+perimetro_romboide=2*base+2*altura
+
+#mostrar datos
+print("la base del romboide es:", base)
+print("la altura del romboide es:", altura)
+print("el perimetro del romboide es:", perimetro_romboide)
