220bf2613614a30b8cb704e883c3d3ccf39d7b8d
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-02 23:04:06 -0500

    Create ejercicio-nro15.py

---
+++ ejercicio-nro15.py
@@ -0,0 +1,16 @@
+#calculadora nro15
+#hallar el volumen de una piramide
+
+#declaracion de variables
+pi,arista,altura,volumen_piramide=0.0,0.0,0.0,0.0
+
+#calculadora
+pi=3.1416
+arista=12.64
+altura=35.15
+volumen_piramide=(pi(radio**2)*h)/3
+
+#mostrar datos
+print("el arista es:", arista)
+print("la altura es:", altura)
+print("el volumen de la piramide es:", volumen_piramide)
