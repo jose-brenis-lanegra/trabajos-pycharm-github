dd04308c61f3c97ac82130af3ae4dafc225a8f4a
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-02 22:58:44 -0500

    Create ejercicio-nro11.py

---
+++ ejercicio-nro11.py
@@ -0,0 +1,17 @@
+#calculadora nro11
+#hallar el volumen del prisma
+
+#declaraciones de variables
+largo,ancho,altura,volumen_prisma=0.0,0.0,0.0,0.0
+
+#calculadora
+largo=17.76
+ancho=3.68
+altura=20.34
+volumen_prisma=largo*ancho*altura
+
+#mostrar datos
+print("el largo es:", largo)
+print("el ancho es:", ancho)
+print("la altura es:", altura)
+print("el volumen del prisma es:", volumen_prisma)
