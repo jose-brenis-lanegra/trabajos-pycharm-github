70c887ba68a51e6ebb90bc4c47ffa578fac98edd
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-02 22:38:44 -0500

    Create ejercicio-nro1.py

---
+++ ejercicio-nro1.py
@@ -0,0 +1,15 @@
+#calculadora nro1
+#hallar el valor del area del triangulo
+
+#declaracion de variables
+base,altura,area_triangulo=0.0,0.0,0.0
+
+#calculadora
+base=5.16
+altura=7.54
+area_triangulo=(base*altura)/2
+
+#mostrar datos
+print("la base es:", base)
+print("la altura es:", altura)
+print("el area del triangulo es:", area_triangulo)
