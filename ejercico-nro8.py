6e0cb9ca15cdaf93386eaafeb70b15cc6c93c220
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-02 22:48:54 -0500

    Create ejercico-nro8.py

---
+++ ejercico-nro8.py
@@ -0,0 +1,16 @@
+#calculadora nro8
+#calcular el area de un elipse
+
+#declaraciones de variables
+pi,semieje_menor,semieje_mayor,area_elipse=0.0,0.0,0.0,0.0
+
+#calculadora
+pi=3.1416
+semieje_menor=45.54
+semieje_mayor=61.93
+area_elipse=pi*semieje_mayor*semieje_menor
+
+#mostrar datos
+print("el semieje menor es:", semieje_menor)
+print("el semieje mayor es:", semieje_mayor)
+print("el area del elipse es:", area_elipse)
