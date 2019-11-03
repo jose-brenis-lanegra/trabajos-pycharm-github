ec8fe370ca03e8f9d33ec8029320a53f71916cb9
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-02 23:13:35 -0500

    Create ejercicio-nro19.py

---
+++ ejercicio-nro19.py
@@ -0,0 +1,15 @@
+#calculdora nro19
+#hallar el area y volumen de un icosaedro
+
+#declaracion de valores
+arista,area_icosaedro,volumen_icosaedro=0.0,0.0,0.0
+
+#calculadora
+arista=36.71
+area_icosaedro=5*(3**(1/2))*(arista**2)
+volumen_icosaedro=(5*(3+(5**(1/2)))*(arista**3))/12
+
+#mostrar datos
+print("la arista es:", arista)
+print("el area del icosaedro es:", area_icosaedro)
+print("el volumen del icosaedro es:", volumen_icosaedro)
