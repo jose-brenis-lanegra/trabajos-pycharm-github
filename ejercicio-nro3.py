e913617fc9f0581f82a9fa3f1a241bb75aab6026
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-02 22:41:47 -0500

    Create ejercicio-nro3.py

---
+++ ejercicio-nro3.py
@@ -0,0 +1,15 @@
+#calculadora nro3
+#hallar el area del rombo
+
+#declaracion de variables
+diagonal_mayor,diagonal_menor,area_rombo=0.0,0.0,0.0
+
+#calculadora
+diagonal_menor=10.89
+diagonal_mayor=39.75
+area_rombo=(diagonal_mayor*diagonal_menor)/2
+
+#mostrar datos
+print("el diagonal mayor es:", diagonal_mayor)
+print("el diagonal menor es:", diagonal_menor)
+print("el area del rombo es:", area_rombo)
