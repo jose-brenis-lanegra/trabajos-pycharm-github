65090131093c620e976670ecb39dd4b8be3519f0
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-02 22:48:15 -0500

    Create ejercicio-nro7.py

---
+++ ejercicio-nro7.py
@@ -0,0 +1,14 @@
+#calculadora nro7
+#hallar el area del circulo
+
+#declaraciones de variables
+pi,radio,area_circulo=0.0,0.0,0.0
+
+#calculadora
+pi=3.1416
+radio=30.67
+area_circulo=pi*(radio**2)
+
+#mostrar datos
+print("el radio del circulo es:", radio)
+print("el area del circulo es:", area_circulo)
