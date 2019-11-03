9a4b59716ced3c43d9520a0302e47db48a5f227a
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-02 23:11:11 -0500

    Create ejercicio-nro17.py

---
+++ ejercicio-nro17.py
@@ -0,0 +1,15 @@
+#calculadora nro17
+#hallar el area y el volumen de un octaedro
+
+#declaracion de valores
+arista,area_octaedro,volumen_octaedro=0.0,0.0,0.0
+
+#calculadora
+arista=6.17
+area_octaedro=2*(3**(1/2))*(arista**2)
+volumen_octaedro=((2**(1/2))*(arista**3))/3
+
+#mostrar datos
+print("la arista es:", arista)
+print("el area del octaedro es:", area_octaedro)
+print("el volumen del octaedro es:", volumen_octaedro)
