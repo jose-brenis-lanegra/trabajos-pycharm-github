8ae7f9a5cb773acf209f011e1d234f7c6b78bdde
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-02 23:05:22 -0500

    Create ejercicio-nro16.py

---
+++ ejercicio-nro16.py
@@ -0,0 +1,15 @@
+#calculadora nro16
+#hallar la altura y volumen de un tetraedro
+
+#declaracion de valores
+arista,altura,volumen_tetraedro=0.0,0.0,0.0
+
+#calculadora
+arista=4.67
+altura=((6**(1/2))*arista)/3
+volumen_tetraedro=((2**(1/2))*(arista**3))/12
+
+#mostrar datos
+print("el arista es:", arista)
+print("la altura del tetraedro es:", altura)
+print("el volumen del tetraedro es:", volumen_tetraedro)
