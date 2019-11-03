e8881979bc94d29a685279473bfc0884da257f09
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-02 22:57:52 -0500

    Create ejercicio-nro10.py

---
+++ ejercicio-nro10.py
@@ -0,0 +1,15 @@
+#calculadora nro10
+#hallar el area y volumen de un cubo
+
+#declaraciones de variables
+arista,area_cubo,volumen_cubo=0.0,0.0,0.0
+
+#calculadora
+arista=6.26
+area_cubo=6*(arista**2)
+volumen_cubo=arista**3
+
+#mostrar datos
+print("la arista es:", arista)
+print("el area del cubo es:", area_cubo)
+print("el volumen del cubo es:", volumen_cubo)
