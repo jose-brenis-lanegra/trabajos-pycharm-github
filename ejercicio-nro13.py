38ab3171b372134d1a8717a6c3098a7e2468947f
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-02 23:00:26 -0500

    Create ejercicio-nro13.py

---
+++ ejercicio-nro13.py
@@ -0,0 +1,16 @@
+#calculadora nro13
+#hallar el area total del cilindro
+
+#declaracion de variables
+pi,altura,radio,area_total_cilindro=0.0,0.0,0.0,0.0
+
+#calculadora
+pi=3.1416
+altura=4.94
+radio=3.54
+area_total_cilindro=2*pi*radio*(radio + altura)
+
+#mostrar datos
+print("la altura es:", altura)
+print("el radio es:", radio)
+print("el area total del cilindro es:", area_total_cilindro)
