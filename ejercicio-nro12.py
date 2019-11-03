76531967d6f4bd56a1eac0807cf3562be7d10918
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-11-02 22:59:29 -0500

    Create ejercicio-nro12.py

---
+++ ejercicio-nro12.py
@@ -0,0 +1,14 @@
+#calculadora nro12
+#hallar el volumen de una esfera
+
+#declaracion de variables
+pi,radio,volumen_esfera=0.0,0.0,0.0
+
+#calculadora
+pi=3.1416
+radio=9.30
+volumen_esfera=(4*pi*(radio**3))/3
+
+#mostrar datos
+print("el radio de la esfera es:", radio)
+print("el volumen de esta esfera es:", volumen_esfera)
